0322eee732fabe4a43c45951a2d4454789bfc4c7
unknown <panyushev@nextmail.ru>
2015-10-15 18:59:47 +0400

    second homework+ last task

--- homework/hw2/example.py
+++
@@ -1,2 +0,0 @@
-seq = ("a", "b", "c"); # This is sequence of strings.
-print ('-'.join( "a","b", "c" ))
--- homework/hw2/task4.py
+++ homework/hw2/task4.py
@@ -1,13 +1,27 @@
-list_of_ints = [input().split(' ')]
-for i in list_of_ints:
-    i = int(list_of_ints[i])
-print (list_of_ints)
 def euclid(first, second):
-    if first < second: first, second = second, first # меняем аргументы местами, если надо
     if first % second == 0:
         return second
     else:
         return euclid(second, first % second)
 
 
+def rpfilter(x, args):
+    list_primes = []
+    for i in range(len(args)):
+        if euclid(x, args[i]) == 1:
+            list_primes.append(args[i])
+    if list_primes == []:
+        return None
+    else:
+        return list_primes
+
+list_of_ints = input().split(' ')
+list_of_ints = [int(s) for s in list_of_ints]
+
+
+x = (rpfilter(list_of_ints[0], list_of_ints))
+if x is not None:
+    print (' '.join(str(x[v]) for v in range(len(x))))
+else:
+    print (None)
 
