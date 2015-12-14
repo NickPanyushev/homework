0c5301ec215d6f7b4354bbcb9a5418f0a67e2816
NickPanyushev <md4UD8vNxAS2>
2015-12-14 19:14:31 +0300

    wiki gameover

--- homework/hw6/task1.py
+++ homework/hw6/task1.py
@@ -12,7 +12,7 @@ def wikilinks(url):
     for element in tree.iter('a'):
         if element.attrib.get('href'):
             links.append(element.attrib.get('href'))
-    regexp = re.compile('\/wiki\/.*')
+    regexp = re.compile('/wiki/')
     filtered_links = []
     for i in range(len(links)):
         if regexp.match(links[i]):
@@ -20,21 +20,20 @@ def wikilinks(url):
     return filtered_links
 
 
-def clicks(Url_start, Url_end, number_clicks):
+def clicks(url_start, url_end, number_clicks):
     if number_clicks == 0:
         return None
     links = []
     print(links)
-    links_start = wikilinks(Url_start)
-    if Url_end in links_start:
-        links = [Url_start, Url_end]
+    links_start = wikilinks(url_start)
+    if url_end in links_start:
+        links = [url_start, url_end]
     else:
         for k in links_start:
-            res = clicks(k, Url_end, number_clicks - 1)
+            res = clicks(k, url_end, number_clicks - 1)
             if res:
-                links = [Url_start] + res
+                links = [url_start] + [res]
                 break
     return '\n'.join(links)
-
 
 print(clicks('https://en.wikipedia.org/wiki/Gone_Maggie_Gone', 'https://en.wikipedia.org/wiki/Theia_(planet)', 3))
