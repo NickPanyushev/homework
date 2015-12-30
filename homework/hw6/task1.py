5e4a39d0801cc1ca427acedf55c7d7d37d63575b
NickPanyushev <md4UD8vNxAS2>
2015-12-31 02:07:43 +0300

    Binary Search Tree

--- homework/hw6/task1.py
+++ homework/hw6/task1.py
@@ -1,5 +1,4 @@
 import re
-
 import requests
 from lxml import etree
 
---
+++ math/.idea/BST.py
@@ -0,0 +1,70 @@
+class BST_Node:
+
+    def __init__(self, data = None):
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def __repr__(self):
+        node = "Node %s left %s right %s " % (self.data, self.left, self.right)
+        return node
+
+    def get_data(self):
+        return self.data
+
+    def get_right(self):
+        return self.right
+
+    def get_left(self):
+        return self.left
+
+    def set_right(self, new_right):
+        self.right = new_right
+
+    def set_left(self, new_left):
+        self.left = new_left
+
+
+def build_BST(array, root = None):
+    if not root:
+        root = BST_Node(array[len(array)//2])
+    for i in array:
+        if root.get_data() == i:
+            continue
+        if root.get_data() > i:
+            if root.get_right() == None:
+                root.set_right(BST_Node(i))
+            else:
+                build_BST([i], root.get_right())
+        if root.get_data() < i:
+            if root.get_left() == None:
+                root.set_left(BST_Node(i))
+            else:
+                build_BST([i], root.get_left())
+    return root
+
+
+a = [1,2,3,4,5,6,7,8,9,10]
+tree = (build_BST(a))
+
+
+def search (root, element):
+    if root.get_data() == element:
+        return root
+    if root.get_data() > element:
+        #print(root.get_left())
+        if root.get_right() == None:
+            return None
+        search (root.get_right(), element)
+    if root.get_data() < element:
+        #print(root.get_left())
+        #print(root.get_data())
+        if root.get_left() == None:
+            return None
+        search (root.get_left(), element)
+
+print (search(tree, 7))
+
+
+
+
