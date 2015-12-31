5e979b5760a0f524bbea2adf1e7f1f9ebd93828b
NickPanyushev <md4UD8vNxAS2>
2015-12-31 21:39:25 +0300

    Binary Search Tree

--- math/.idea/BST.py
+++ math/.idea/BST.py
@@ -1,6 +1,5 @@
 class BST_Node:
-
-    def __init__(self, data = None):
+    def __init__(self, data=None):
         self.data = data
         self.right = None
         self.left = None
@@ -25,9 +24,9 @@ class BST_Node:
         self.left = new_left
 
 
-def build_BST(array, root = None):
+def build_BST(array, root=None):
     if not root:
-        root = BST_Node(array[len(array)//2])
+        root = BST_Node(array[len(array) // 2])
     for i in array:
         if root.get_data() == i:
             continue
@@ -44,27 +43,18 @@ def build_BST(array, root = None):
     return root
 
 
-a = [1,2,3,4,5,6,7,8,9,10]
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 tree = (build_BST(a))
 
 
-def search (root, element):
+def search(root, element):
     if root.get_data() == element:
         return root
     if root.get_data() > element:
-        #print(root.get_left())
         if root.get_right() == None:
             return None
-        search (root.get_right(), element)
+        search(root.get_right(), element)
     if root.get_data() < element:
-        #print(root.get_left())
-        #print(root.get_data())
         if root.get_left() == None:
             return None
-        search (root.get_left(), element)
-
-print (search(tree, 7))
-
-
-
-
+        return search(root.get_left(), element)
