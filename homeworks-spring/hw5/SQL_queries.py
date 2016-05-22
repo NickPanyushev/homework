e392565035d8d74f43db03857fda057eedcb73f5
nickolay <panyushev@nextmail.ru>
2016-05-23 00:07:17 +0300

    +SQL

--- homeworks-spring/hw5/SQL_queries.py
+++ homeworks-spring/hw5/SQL_queries.py
@@ -3,4 +3,10 @@ import sqlite3
 db = sqlite3.connect('data.sqlite')
 cursor = db.cursor()
 
-t = cursor.execute('select * from Goods limit 5').fetchall()
+t = cursor.execute().fetchall()
+
+
+def unpaid(user_id):
+
+
+return data
