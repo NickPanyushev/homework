0e867910f7ef13383ee85671b273a182d3d541d8
NickPanyushev <md4UD8vNxAS2>
2016-06-14 16:17:23 +0300

    + test2

--- mine/MMPs_downloader.py
+++ mine/MMPs_downloader.py
@@ -13,16 +13,18 @@ parser.add_argument("gene",
 
 parser.add_argument("type",
                     nargs='?',
+                    metavar='type',
                     default="Genomic",
                     type=str,
                     choices=["Genomic", "Transcript", "Protein"],
-                    help="Type of the biomolecule to find")
+                    help="Type of the biomolecule to find:"
+                         " Genomic, Transcript, Protein")
 
 parser.add_argument("-refseq",
                     nargs='?',
-                    type=bool,
-                    default=False,
-                    # choices=[True, False],
+                    type=str,
+                    default="n",
+                    choices=["y","n"],
                     help="Search only in RefSeq database")
 
 parser.add_argument("-orgn",
@@ -31,13 +33,10 @@ parser.add_argument("-orgn",
                     help="Organism to search sequences for")
 
 args = parser.parse_args()
-print(args)
 gene = args.gene
 mol_type = args.type
 refseq = args.refseq
 organism = args.orgn
-
-
 
 Entrez.email = 'panyushev@nextmail.ru'
 
@@ -45,37 +44,32 @@ term = gene + '[GENE]'
 
 if organism:
     term = term + ' AND ' + organism + '[ORGN]'
-    print(term)
 
-if refseq:
+
+if refseq == "y":
     term = term + ' AND refseq[filter]'
-    print(term)
 
-if mol_type == "Genomic":
+if mol_type == "Protein":
+    handle = Entrez.esearch(db='protein', term=term)
+
+elif mol_type == "Genomic":
     term = term + " AND biomol_genomic[PROP]"
-    print(term)
+    handle = Entrez.esearch(db='nucleotide', term=term)
 
 elif mol_type == "Transcript":
     term = term + " AND (biomol_mrna[PROP] OR biomol_rrna[PROP] OR biomol_crna[PROP]" \
                   " OR biomol_scrna[PROP] OR biomol_snrna[PROP] OR biomol_snorna[PROP] OR biomol_trna[PROP])"
-    print(term)
-
-    # handle = Entrez.esearch(db='nucleotide', term=term)
-
-elif mol_type == "Protein":
-    print(term)
-
-    #handle = Entrez.esearch(db='protein', term=term)
+    handle = Entrez.esearch(db='nucleotide', term=term)
 
 
-# result = Entrez.read(handle)
 
+result = Entrez.read(handle)
+#pprint(result)
 
-# print(result)
-
-# for i in result['IdList']:
+for i in result['IdList']:
+    print(i)
 #     ID_output = Entrez.esummary(db='nucleotide', id=i)
 #     out_dict = Entrez.read(ID_output)[0]
 # print(out_dict)
-# out = [out_dict['Accession'], out_dict['taxon'], out_dict['title']]
-# print('\t'.join(out))
+#out = [out_dict['Accession'], out_dict['taxon'], out_dict['title']]
+#print('\t'.join(out))
